4573651cf020e149fc70de28c0a079022a7abee5
Phuong Tran <thphuong204@gmail.com>
2022-11-28 16:55:37 +0700

    revise routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+const userAPI = require('./users.api');
+router.use('/users', userAPI);
+const taskAPI = require('./tasks.api');
+router.use('/tasks', taskAPI)
+
 module.exports = router;
